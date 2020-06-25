fa513266b903c372d397b470995658c5e05d28c2
Anh <tnanh2501199777@gmail.com>
2020-06-25 12:04:30 +0700

    statistic

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -23,7 +23,7 @@ class StatisticController extends Controller
         if($request->selectmonth){
             $month = $request->selectmonth;
         }
-        $thongke=DB::table("bills")->select(DB::raw("MONTH(created_at) as Thang")  ,DB::raw("(SUM(total)) as Tong"))->groupBy(DB::raw("MONTH(created_at)"))->where('tinhtrang',1)->whereYear('created_at', $year)->get();
+        $thongke=DB::table("bills")->select(DB::raw("MONTH(date_order) as Thang")  ,DB::raw("(SUM(total)) as Tong"))->groupBy(DB::raw("MONTH(date_order)"))->where('tinhtrang',1)->whereYear('date_order', $year)->get();
         
         $seller = DB::table("products")->join('bill_detail','products.id','=','bill_detail.id_product')->join('bills','bills.id','=','bill_detail.id_bill')->select(DB::raw("products.id"), DB::raw("products.name"),DB::raw("products.unit_price"), DB::raw("products.promotion_price"),DB::raw("products.slug"),DB::raw("SUM(bill_detail.quantity) AS soluong"))->where('bills.tinhtrang',1)->groupBy(DB::raw("products.id"), DB::raw("products.name"),DB::raw("products.unit_price"), DB::raw("products.promotion_price"),DB::raw("products.slug"))->orderByRaw('SUM(bill_detail.quantity) DESC')->whereYear('bill_detail.created_at', 2020)->whereMonth('bill_detail.created_at', $month)->get();
         
